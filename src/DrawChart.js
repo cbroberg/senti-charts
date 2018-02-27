085d495508106ac0221cb3da101e3eb39d66f683
Dom <1034971@ucn.dk>
2018-02-27 12:40:20 +0100

    conditional rendering

--- src/DrawChart.js
+++ src/DrawChart.js
@@ -9,10 +9,26 @@ export default class Chart extends Component {
         super(props);
 
         this.state = {
-            activeIndex: 0
+            activeIndex: 0,
+            pv: false,
+            uv: false,
+            amt: false,
         }
-
         this.handleClick = this.handleClick.bind(this);
+        this.handleLegendClick = this.handleLegendClick.bind(this);
+        this.handleCheckBoxClick = this.handleCheckBoxClick.bind(this);
+    }
+
+    handleCheckBoxClick(event) {
+        // const checkbox = event.currentTarget.value
+        // console.log(event.currentTarget)
+        // this.setState({
+        //     checkbox: !this.state.checkbox
+        // });
+
+        this.setState({
+            [event.currentTarget.value]: event.target.checked
+        })
     }
 
     handleClick(data, index) {
@@ -23,8 +39,8 @@ export default class Chart extends Component {
 
     handleLegendClick(event) {
         this.setState({
-            activeIndex: event.currentTarget.dataset.id
-        })
+            activeIndex: parseInt(event.currentTarget.dataset.id)
+        });
     }
 
     render() {
@@ -37,7 +53,7 @@ export default class Chart extends Component {
             { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
             { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
         ];
-        const { activeIndex } = this.state;
+        const { activeIndex, pv, uv, amt } = this.state;
         const activeItem = data[activeIndex];
 
         const renderLegend = (props) => {
@@ -45,16 +61,27 @@ export default class Chart extends Component {
                 <ul>
                     {
                         props.payload.map((entry, index) => (
-                            <li cursor="pointer" key={`item-${index}`} fill={index === activeIndex ? '#ff0000' : '#8884d8'} data-id={index} onClick={this.handleLegendClick.bind(this)}>{entry.name}</li>
+                            <li cursor="pointer" key={`item-${index}`} fill='#8884d8' data-id={index} onClick={this.handleLegendClick}>{entry.name}</li>
                         ))
                     }
                 </ul>
             );
         }
-        console.log(activeIndex)
 
         return (
             <div>
+                <div>
+                    <input type="checkbox" id="pv" name="checkbox" value="pv" onClick={this.handleCheckBoxClick}/>
+                    <label forhtml="pv">Pv</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="uv" name="chekbox" value="uv" onClick={this.handleCheckBoxClick}/>
+                    <label forhtml="uv">Uv</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="amt" name="chekbox" value="amt" onClick={this.handleCheckBoxClick} />
+                    <label forhtml="amt">Amt</label>
+                </div>
             <BarChart width={700} height={300} data={data}
                 margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                 <XAxis dataKey="name" />
@@ -68,15 +95,16 @@ export default class Chart extends Component {
                     content={renderLegend}
                 >
                 </Legend>
-                <Bar dataKey="pv" fill="#CE6629" onClick={this.handleClick}>
-                {
-                    data.map((entry, index) => (
-                            <Cell cursor="pointer" fill={index === activeIndex ? '#ff0000' : '#8884d8'} key={`cell-${index}`} />
-                    ))
-                }
-                </Bar>
-                <Bar dataKey="uv" fill="#1DA493" />
-                <Bar dataKey="amt" fill="#CEB42B" />
+                {pv ? <Bar dataKey="pv" fill="#CE6629" onClick={this.handleClick}>
+                    {
+                        data.map((entry, index) => (
+                               <Cell fill='#8884d8' key={`cell-${index}`} />
+                        ))
+                    }
+                    </Bar> : ''}
+
+                {uv ? <Bar dataKey="uv" fill="#1DA493" /> : ''}
+                {amt ? <Bar dataKey="amt" fill="#CEB42B" /> : ''}
             </BarChart>
             <p className="content">{`Uv of "${activeItem.name}": ${activeItem.uv}`}</p>
             </div>
